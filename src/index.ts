19a3c9c97f5a3f88afc69475a2cd150fc0996307
Renato <renato.melo@ab-inbev.com>
2025-06-18 22:09:49 -0300

    chore: add host to expose port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ const app = fastify({
 app.register(router)
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 4000,  })
+  await app.listen({
+    port: Number(process.env.PORT) || 4000,
+    host: '0.0.0.0'
+  })
 } catch (error) {
   app.log.error(error);
   process.exit(1);  
